a9b9975731e4f0b00126dea5b89f4efd33d8c760
Pietro Marchini <pietro.marchini94@gmail.com>
2025-08-10 12:05:33 +0200

    fix: css style

--- theme/layouts/default.vue
+++ theme/layouts/default.vue
@@ -3,23 +3,3 @@
     <slot />
   </div>
 </template>
-
-<style>
-/* Layout-specific styles are handled in theme/styles/index.css */
-.slidev-layout.default {
-  padding: 2.5rem 3.5rem;
-  font-size: 1.1rem;
-  position: relative;
-}
-
-/* Logo styles - making sure they apply */
-.slidev-layout.default .logo {
-  position: absolute !important;
-  top: 2rem !important;
-  right: 2rem !important;
-  width: 4rem !important;
-  height: 4rem !important;
-  object-fit: contain !important;
-  z-index: 10 !important;
-}
-</style>
